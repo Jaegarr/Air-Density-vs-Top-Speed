21bd1d2980fd28941fafc99d53e5e6a8af0e36ad
Jaegarr <berke.s.bayram@gmail.com>
2025-05-26 12:58:59 +0100

    Update AirDensity_vs_MaximumSpeed.m

--- AirDensity_vs_MaximumSpeed.m
+++ AirDensity_vs_MaximumSpeed.m
@@ -1,101 +1,92 @@
-%% Track Data
-Track = {'Bahrain'; 'Jeddah'; 'Melbourne'; 'Suzuka'; 'Shanghai';'Miami'; 'Imola'; 'Monaco'; ...
-    'Montreal'; 'Barcelona';'Spielberg'; 'Silverstone'; 'Budapest'; 'Spa'; 'Zandvoort';'Monza'; ... 
-    'Baku'; 'Singapore'; 'Austin'; 'Mexico City';'São Paulo'; 'Las Vegas'; 'Lusail'; 'Yas Marina'};
-Elevation = [7; 3; 31; 35; 3; 3; 35; 10; 10; 58; 660; 170; 100; 368; 5; 162; -28; 15; 225; 2240; 784; 630; 5; 5]; %In meters
-AvgTemp  = [30; 32; 18; 25; 22; 29; 20; 28; 22; 26; 20; 17; 30; 18; 17; 24; 35; 30; 28; 16; 22; 35; 42; 32]; % In degree Celcius
-data = table(Track, Elevation, AvgTemp);
-%% Properties
-%Universal constants
-p0 = 101325; % Sea-level standard pressure [Pa]
-H_scale = 8434;  % Scale height [m] for barometric formula
-g = 9.81; % Gravity [m/s^2]
-R = 287; % Specific gas constant for air [J/(kg·K)]
-gamma = 1.4; % Heat capacity ratio
-% Power Unit parameters
-Vd = 0.0016; % Displacement volume [m^3], Article xxx
-RPM_max = 12500; % Maximum engine speed [RPM], used the rev limiter value from Article xxx
-AFR = 14.7; % Stoichiometric AFR 
-LHV_g= 44e6; % Lower heating value of gasoline [J/kg]
-LHV_e= 39e6; % Lower heating value of ethanol [J/kg]
-n_combustion = 0.50; % Combustion efficiency (assumed), this number is thrown around a lot so why not?
-n_MGUK = 0.88; % This efficiency value is taken from the efficiency map I have used in my SAE publication for 12500 RPM
-inlet_d= 345e-4; % Inlet valve diameter, 2026 Regulations C5.4.13, Couldn't find information for 2025 cars so assumed safe
-P_MGUK_2025 = 120e3; % MGU-K power limit 2025 [W]
-P_MGUK_2026 = 350e3; % MGU-K power limit 2026 [W]
-% Vehicle Characteristics 
-Cd     = 0.78; % Drag coefficient
-Af = 1.5;         % frontal area [m^2]
-Cr = 0.01;    % Rolling resistance coefficient, this value is taken from my SAE publication
-mass_2025 = 800; % 2025 Vehicle mass including driver [kg], Article 4.1
-mass_2026 = 768;  % 2026 Vehicle mass including driver [kg], Article 4.1
+%% Data Input
+tracks = { ...
+    'Bahrain','Jeddah','Melbourne','Suzuka','Shanghai','Miami', ...
+    'Imola','Monaco','Montreal','Barcelona','Spielberg','Silverstone', ...
+    'Budapest','Spa','Zandvoort','Monza','Baku','Singapore','Austin', ...
+    'Mexico City','São Paulo','Las Vegas','Lusail','Yas Marina'}';
+elevation_m = [7;3;31;35;3;3;35;10;10;58;660;170;100;368;5;162;-28;15;225;2240;784;630;5;5];
+avgTemp_C = [30;32;18;25;22;29;20;28;22;26;20;17;30;18;17;24;35;30;28;16;22;35;42;32];
 
-%% Preallocate results
-n = height(data);
-rho = zeros(n,1);
-mdot_air = zeros(n,1);
-mdot_fuel = zeros(n,1);
-P_ICE = zeros(n,1);
-P_ICE_e = zeros(n,1);
-P_tot_2025 = zeros(n,1);
-P_tot_2026 = zeros(n,1);
-P_tot_2026_e = zeros(n,1);
-v_max_2025 = zeros(n,1);
-v_max_2026 = zeros(n,1);
-v_max_2026_e = zeros(n,1);
-%% 
-for i = 1:n
-    % Ambient conditions
-    T_K = data.AvgTemp(i) + 273.15;
-    p_track = p0 * exp(-data.Elevation(i) / H_scale);
-    rho(i) = p_track / (R * T_K);
-    % Air mass flow [kg/s]
-    mdot_air(i) = rho(i)*pi/4*inlet_d^2*(gamma*R*T_K)^0.5;  % Maximum flow condition at Mach 1
-    % Fuel mass flow [kg/s]
-    mdot_fuel(i) = mdot_air(i) / AFR;
-    % ICE power [W]
-    P_ICE(i) = mdot_fuel(i) * LHV_g * n_combustion;
-    P_ICE_e(i) = mdot_fuel(i) * LHV_e * n_combustion;
-    % Total peak power [W]
-    P_tot_2025(i) = P_ICE(i) + P_MGUK_2025*n_MGUK;
-    P_tot_2026(i) = P_ICE(i) + P_MGUK_2026*n_MGUK;
-    P_tot_2026_e(i) = P_ICE_e(i) + P_MGUK_2026*n_MGUK;
-    % Solve for top speed [m/s]: P = F_drag + F_rr * v
-    f_speed = @(v,P) 0.5 * rho(i) * (Cd*Af) * v.^3 + Cr * mass_2026 * g * v - P;
-    v_max_2025(i) = fzero(@(v) f_speed(v, P_tot_2025(i)), 100);
-    v_max_2026(i) = fzero(@(v) f_speed(v, P_tot_2026(i)), 100);
-    v_max_2026_e(i) = fzero(@(v) f_speed(v, P_tot_2026_e(i)), 100);
+data = table(tracks, elevation_m, avgTemp_C, 'VariableNames', ...
+    {'Track','Elevation_m','AvgTemp_C'});
+
+%% Universal constants (SI)
+p0_Pa       = 101325;           % Sea-level pressure
+Hscale_m    = 8434;             % Scale height
+g_m_s2      = 9.81;             % Gravity
+R_J_kgK     = 287;              % Gas constant
+gamma       = 1.4;              % Heat capacity ratio
+
+%% Power-unit parameters
+Vd_m3       = 0.0016;
+RPM_max     = 12500;
+AFR         = 14.7;
+LHV_g_J_kg  = 44e6;
+LHV_e_J_kg  = 39e6;
+eta_comb    = 0.50;
+eta_MGUK    = 0.88;
+inlet_d_m   = 345e-4;
+P_MGUK25_W  = 120e3;
+P_MGUK26_W  = 350e3;
+
+%% Vehicle characteristics
+Cd          = 0.78;
+Af_m2       = 1.5;
+Cr          = 0.01;
+mass25_kg   = 800;
+mass26_kg   = 768;
+
+%% Compute ambient densities and mass flows (vectorized)
+T_K = data.AvgTemp_C + 273.15;
+p_Pa = p0_Pa .* exp(-data.Elevation_m ./ Hscale_m);
+rho = p_Pa ./ (R_J_kgK .* T_K);
+mdot_air = rho .* (pi/4*inlet_d_m^2) .* sqrt(gamma * R_J_kgK .* T_K);
+mdot_fuel = mdot_air ./ AFR;
+
+%% Power calculations
+P_ICE25 = mdot_fuel .* LHV_g_J_kg .* eta_comb;
+P_ICE26 = mdot_fuel .* LHV_g_J_kg .* eta_comb;
+P_ICE26e= mdot_fuel .* LHV_e_J_kg .* eta_comb;
+
+P_tot25 = P_ICE25 + P_MGUK25_W .* eta_MGUK;
+P_tot26 = P_ICE26 + P_MGUK26_W .* eta_MGUK;
+P_tot26e= P_ICE26e + P_MGUK26_W .* eta_MGUK;
+
+%% Top-speed solver (vectorized via arrayfun)
+solveVmax = @(Ptot,mass) arrayfun(@(rho_i,P_i) ...
+    fzero(@(v) dragRR(v,rho_i,Cd,Af_m2,Cr,mass,g_m_s2) - P_i, 100), ...
+    rho, Ptot);
+
+v25 = solveVmax(P_tot25, mass25_kg);
+v26 = solveVmax(P_tot26, mass26_kg);
+v26e= solveVmax(P_tot26e,mass26_kg);
+
+%% Append results
+data.Rho = rho;
+data.MdotAir = mdot_air;
+data.MdotFuel= mdot_fuel;
+data.Power25_kW = P_tot25/1e3;
+data.Power26_kW = P_tot26/1e3;
+data.Power26e_kW= P_tot26e/1e3;
+data.Vmax25_kph = v25*3.6;
+data.Vmax26_kph = v26*3.6;
+data.Vmax26e_kph= v26e*3.6;
+
+%% Plotting
+figure;
+tl = tiledlayout(2,1,'Padding','compact','TileSpacing','compact');
+nexttile;
+plot(categorical(data.Track), data.Power25_kW, 'DisplayName','2025'); hold on;
+plot(categorical(data.Track), data.Power26_kW, 'DisplayName','2026');
+plot(categorical(data.Track), data.Power26e_kW,'DisplayName','2026 Eth');
+ylabel('Total Power [kW]'); legend('Location','southwest');
+nexttile;
+plot(categorical(data.Track), data.Vmax25_kph, 'DisplayName','2025'); hold on;
+plot(categorical(data.Track), data.Vmax26_kph, 'DisplayName','2026');
+plot(categorical(data.Track), data.Vmax26e_kph,'DisplayName','2026 Eth');
+ylabel('Top Speed [kph]'); xtickangle(45); legend('Location','southwest');
+
+%% Drag + Rolling-Resistance function
+function F = dragRR(v,rho,Cd,Af,Cr,mass,g)
+    F = 0.5*rho*(Cd*Af)*v.^3 + Cr*mass*g.*v;
 end
-%% Results
-data.Rho_kg_m3 = rho;
-data.MdotAir_kg_s = mdot_air;
-data.MdotFuel_kg_s = mdot_fuel;
-data.Power_ICE_kW = P_ICE/1e3;
-data.Power_total2025_kW = P_tot_2025/1e3;
-data.Power_total2026_kW = P_tot_2026/1e3;
-data.Power_total2026_e_kW = P_tot_2026_e/1e3;
-data.Vmax2025_kph = v_max_2025 * 3.6;
-data.Vmax2026_kph = v_max_2026 * 3.6;
-data.Vmax2026_e_kph = v_max_2026_e*3.6;
-%% Plots
-trackNames = categorical(data.Track);
-trackNames = reordercats(trackNames, data.Track);  % preserve original order
-%Validation
-%Vmax2024 = [332.2;0;0;0;0;0;0;289.9;0;0,;0;0;0;357.2;0;0;0;0;0;0;0;0;0;0]
-%Accuracy = 1-abs((data.Vmax2025_kph-Vmax2024)/Vmax2024)
-%plot(trackNames,Accuracy)
-%yline(0.95)
-subplot(2,1,1);
-hold on
-p1 = plot(trackNames, data.Power_total2025_kW);
-p2 = plot(trackNames, data.Power_total2026_kW);
-p3 = plot(trackNames, data.Power_total2026_e_kW);
-legend([p1 p2 p3 ],{'2025', '2026', '2026 Ethanol'},'Location', 'southwest');
-ylabel('Power [kW]')
-subplot(2,1,2);
-hold on
-v1 = plot(trackNames, data.Vmax2025_kph);
-v2 = plot(trackNames, data.Vmax2026_kph);
-v3 = plot(trackNames, data.Vmax2026_e_kph);
-legend([v1 v2 v3 ],{'2025', '2026', '2026 Ethanol'},'Location', 'southwest');
-ylabel('Top Speed [kph]')
